33664beaed1c433059f78c2dc3f6742738524a07
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-10-31 13:04:46 -0400

    Good riddance

--- ZeroEye/Pods/Target Support Files/Pods-ZeroEye-ZeroEyeUITests/Pods-ZeroEye-ZeroEyeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZeroEye_ZeroEyeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ZeroEye_ZeroEyeUITests
-@end
--- ZeroEye/Pods/Target Support Files/Pods-ZeroEyeTests/Pods-ZeroEyeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ZeroEyeTests : NSObject
-@end
-@implementation PodsDummy_Pods_ZeroEyeTests
-@end
